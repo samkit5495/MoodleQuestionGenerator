f9a66d2f594276889c856b835ba831660c41597f
Samkit Jain <samkit5495@gmail.com>
2022-08-26 11:25:49 +0530

    chore: update libs to latest version

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ import json
 import traceback
 
 from flask import Flask, request, render_template, redirect, url_for, send_from_directory, Response
+from flask.cli import load_dotenv
 from flask_bootstrap import Bootstrap
 # from werkzeug import secure_filename
 from convertToXML import convert_to_xml
 from lib.upload_file import uploadfile
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -125,4 +128,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=os.environ.get('PORT', 8000))
